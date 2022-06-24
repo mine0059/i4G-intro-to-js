697736cddb48e4ac7678a53620cf36b6179dc6a8
mine0059 <104103066+mine0059@users.noreply.github.com>
2022-06-24 21:49:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+alert("my name is Emmanuel, my height is 5.6ft and am from Nigeria. ");
+alert("How About you can you tell me your: Name, Height, and country");
+
+var Name = prompt("Enter your name:");
+var height = prompt("Enter your height:");
+var country = prompt("Enter your country:");
+
+
+alert("My name is " + Name + ", my height is " + height + "ft and am from " + country);
+alert("Hope you would not mind hanging out for some beer " + Name + "?");
